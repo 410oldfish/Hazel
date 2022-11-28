0161cf59a6da51102beb39dd5b5f05600e6ec217
yuhongyue <yuhongyue@boke.com>
2022-11-28 20:56:16 +0800

    [feat]创建项目

---
+++ Hazel/Hazel/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//for use by hazel applications
+
+#include <stdio.h>
+#include "Application.h"
+
+//--------------------Entry Point------------------------------
+#include "EntryPoint.h"
+
+//------------------------------------------------------------
